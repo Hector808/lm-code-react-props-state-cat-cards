2fc2c7a458848355823f8cdc98f05b6e79762ca6
Hector808 <119350863+Hector808@users.noreply.github.com>
2023-02-02 20:16:10 -0300

    exercises 1 to 9
    exercises 1 to 9

--- src/App.tsx
+++ src/App.tsx
@@ -4,101 +4,61 @@ import Header from './components/header';
 import Footer from './components/footer';
 import { useState } from 'react';
 import Cat from './data/cat';
+import Dog from './data/dog';
 import CatCard from './components/cat_card'
+import DogCard from './components/dog_card'
+import catData from './data/cat-data';
+import dogData from './data/dog-data';
+import AddAnimalForm from './components/form';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
 
-  const [cats, setCats] = useState<Array<Cat>>(
-    
-	[
-		{
-				 name: 'Little Miss Purrfect',
-				 species: 'Cat',
-				 favFoods: ['wet food', 'dry food'],
-				 birthYear: 2016,
-			 },
-			 {
-				 name: 'Mr Gwumpy',
-				 species: 'Cat',
-				 favFoods: ['caviar'],
-				 birthYear: 2016,
-			 },
-			 {
-				 name: 'Dopey',
-				 species: 'Cat',
-				 favFoods: ['bugs'],
-				 birthYear: 2018,
-			 },
-			 {
-				 name: 'Jumpy',
-				 species: 'Scaredy Cat',
-				 favFoods: ['not cucumbers!'],
-				 birthYear: 2008,
-			 },
-			 {
-				 name: 'Meowsalot',
-				 species: 'Cat',
-				 favFoods: ['tuna', 'catnip', 'celery'],
-				 birthYear: 2012,
-			 },
-			 {
-				 name: 'Cindy Clawford',
-				 species: 'Cat',
-				 favFoods: ['mice'],
-				 birthYear: 2012,
-			 },
-			 {
-				 name: 'Katy Purry',
-				 species: 'Cat',
-				 favFoods: ['cigarettes', 'coffee'],
-				 birthYear: 2015,
-			 },
-			 {
-				 name: 'Dr. Von Belly-Rub',
-				 species: 'Cat',
-				 favFoods: ['salt'],
-				 birthYear: 2020,
-			 },
-			 {
-				 name: 'Blobby',
-				 species: 'Bird?',
-				 favFoods: ['your soul'],
-				 birthYear: 2020,
-			 },
-			 {
-				 name: 'El Taco',
-				 species: 'Cat',
-				 favFoods: ['tequila', 'bar snax'],
-				 birthYear: 1995,
-			 },
-			 {
-				 name: 'Nibbles',
-				 species: 'Hungry Cat',
-				 favFoods: ['blankets', 'feet', 'tinsel'],
-				 birthYear: 2003,
-			 },
-			 {
-				 name: 'BoopySnoot',
-				 species: 'Tiny Cat',
-				 favFoods: ['milk'],
-				 birthYear: 2021,
-			 }
-	 ]
-	
-	// big cat array from above is here!
-	
-	);
+function App(): JSX.Element {
 
-	console.log("Our pretties 😻: ", cats)
+	// JavaScript/TypeScript code can be inserted here!
+	const [cats, setCats] = useState<Array<Cat>>(catData);
 
-  return (
-    <>
-      <Navbar />
-      <Header />
+	const [dogs, setDogs] = useState<Array<Dog>>(dogData);
 
-      <main>
-	<div className="cards__wrapper">{cats.map(cat => <CatCard /> )}</div>
-      </main>
+	const catCount = cats.length;
+	const dogCount = dogs.length;
+
+	const addCat = (newCat: Cat) => {
+		newCat.id = uuidv4();
+		setCats([...cats, newCat]);
+	}
+
+	const addDog = (newDog: Dog) => {
+		newDog.id = uuidv4();
+		setDogs([...dogs, newDog]);
+	}
+
+	return (
+		<>
+			<Navbar />
+			<Header catCount={catCount} dogCount={dogCount} />
+
+	  <main>
+				<div className='cards__wrapper'>
+					{cats.map((cat, index) => <CatCard
+						key={cat.id}
+						name={cat.name}
+						species={cat.species}
+						favFoods={cat.favFoods}
+						birthYear={cat.birthYear}
+						catIndex={index}
+					/>)}
+					{dogs.map((dog, index) => <DogCard
+						key={dog.id}
+						name={dog.name}
+						species={dog.species}
+						favFoods={dog.favFoods}
+						birthYear={dog.birthYear}
+						dogIndex={index}
+					/>)}
+				</div>
+				<AddAnimalForm addCat={addCat} addDog={addDog} />
+			</main>
 
       <Footer />
     </>
